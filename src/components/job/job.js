fda7a93aa2b8c004eccec7117d6537bfcb8d81b9
Tobias Ramos <tobiasramossilva@gmail.com>
2023-06-22 21:06:40 -0300

    Atribuindo todos os dados da lista, ainda falta a estilização e a refatoração do código

--- src/components/job/job.js
+++ src/components/job/job.js
@@ -1,14 +1,34 @@
 import data from '../data/data.json'
-
+import './job.css'
 const Job = () => {
 
     return (
-        <div>
+        <div className='container'>
             {data.map((job) => (
-                <div key={job.id}>
-                    <div>
-                        <h3>{job.company}</h3>
-                        <p>{job.position}</p>
+                <div key={job.id} className='card'>
+
+                    <div className='profile'>
+                        <img src={job.logo} />
+
+                        <div className='user-details'>
+                            <span>{job.company}</span> <br></br>
+                            <span>{job.position}</span>
+                            <div className='vacancy-details'>
+                                <span>{job.postedAt}</span>
+                                <span>{job.contract}</span>
+                                <span>{job.location}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='languages'>
+
+                        <ul>
+                            <li>
+                              {job.role} {job.level} {job.languages}
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
             ))}
